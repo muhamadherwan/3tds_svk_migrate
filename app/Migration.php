485cf0d3a9c5d75df50cc37671a991c7a216c6ee
muhamadherwan <herwan@LAPTOP-95SA2SEL>
2022-08-13 18:28:45 +0800

    using static

---
+++ app/Migration.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @author Herwan <mdherwan@gmail.com>
+ * @link -
+ */
+
+declare(strict_types = 1);
+
+class Migration
+{
+    // props
+    public string $name;
+
+    /**
+     * @param mixed
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->name = 'name';
+        // excess static props
+        // self::$counter++;
+    }
+
+    /**
+     * @param string $type PMR or SPM
+     *
+     * @return void
+     */
+    public static function start( string $type )
+    { 
+
+        echo $result = ( preg_match( "/\PMR:\b/", $type ) ) ? self::startPmr( $type ) : self::startSpm( $type );
+
+    }
+
+    /**
+     * @param mixed string $type PMR or SPM
+     *
+     * @return string
+     */
+    public function startPmr( string $type ):string
+    {
+        return "start {$type}{$this->name}";
+    }
+
+    /**
+     * @param mixed string $type PMR or SPM
+     *
+     * @return string
+     */
+    public static function startSpm( string $type ):string
+    {
+        return "start {$type}";
+    }
+
+}
